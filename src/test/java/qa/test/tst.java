789793526d30dd55cf8d6725b14a0b657107b39f
skamat2-oss <60158689+skamat2-oss@users.noreply.github.com>
2020-08-09 22:17:26 -0400

    hjn

---
+++ src/test/java/qa/test/tst.java
@@ -0,0 +1,34 @@
+package qa.test;
+
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class tst {
+
+	
+	@Test
+	public void setsdt()
+	{
+		System.out.println("kadu");
+		Assert.assertEquals("kasu","kasu");
+	}
+	@Test
+	public void stsdst()
+	{
+		System.out.println("kaedu");
+		Assert.assertEquals("kasu","kasu");
+	}
+	@Test
+	public void sdt()
+	{
+		System.out.println("karf");
+		Assert.assertEquals("kasu","kasu");
+	}
+	
+	
+	
+	}
+
+
